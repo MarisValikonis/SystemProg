424a4c6e7cabb73cca912aa58afc3750b1a754ae
maris <marisvalikonis@gmail.com>
2022-04-27 13:35:06 +0700

    april 27

---
+++ script.js
@@ -0,0 +1,50 @@
+let IdCounter = 0;
+const input = document.querySelector('input[type="text"]');
+
+userInput.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (input.value.trim().length != 0) {
+        addTask();
+    }
+});
+
+let addTask = () => {
+    IdCounter++;
+
+    //Recoger Texto
+    let newValue = input.value;
+
+    //Agregar tarea
+    const newTask = `
+        <div class="task-container" id="${IdCounter}">
+            <label>
+                <input type="checkbox"> 
+                ${newValue}
+            </label>
+            <img src="./images/delete.png" class="closeBtn">
+        </div>
+    `;
+    list.innerHTML += newTask;
+    input.value = '';
+    updateStats();
+}
+
+list.addEventListener('click', (event) => {
+    if (event.srcElement.nodeName == 'INPUT') {
+        updateStats();
+    } else if (event.srcElement.nodeName == 'IMG') {
+        deleteTask(event.srcElement.parentNode.id);
+    }
+});
+
+let updateStats = () => {
+    let element = list.querySelectorAll('div');
+    let checkbox = document.querySelectorAll('input[type="checkbox"]:checked');
+    stats.innerHTML = `Всего задач: ${element.length} Выполненые: ${checkbox.length}`;
+};
+
+let deleteTask = (id) => {
+    let taskToDelete = document.getElementById(id);
+    list.removeChild(taskToDelete);
+    updateStats();
+};
